0881975ab48bc7bf782b94591590888adfd937be
Phol <Pholibertlim@gmail.com>
2023-06-14 15:34:27 +0800

    fix: changed middlware authentication of nextauth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,3 @@
-import {withAuth} from 'next-auth/middleware';
-
-//protecting specific paths and will redirect to login if not authenticated
-export default withAuth({
-  pages: {
-    signIn: '/api/auth/signin',
-  },
-});
+export { default } from "next-auth/middleware"
 
 export const config = { matcher: ["/home/:path*"] }
